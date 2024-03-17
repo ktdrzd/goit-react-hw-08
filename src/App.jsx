d252496728957a97f373b41aed9de66fbde4b27a
ktdrzd <137915871+ktdrzd@users.noreply.github.com>
2024-03-17 14:32:48 +0200

    imroved

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
 
 const App = () => {
   const dispatch = useDispatch();
-  // const { isRefreshing } = selectAuth;
-  const isRefreshing = useSelector(selectAuth);
+
+  const {isRefreshing} = useSelector(selectAuth);
 
   useEffect(() => {
     dispatch(refresh());
